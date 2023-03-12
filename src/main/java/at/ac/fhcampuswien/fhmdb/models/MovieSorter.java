771aa7b9703412e91f135d3dfb0d33430073915e
Mellonne <rkizilirmak42@hotmail.com>
2023-03-12 18:25:15 +0100

    Created MovieSorter.java, Ascending and Descent Functionality is there but there is no linkup with the GUI

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieSorter.java
@@ -0,0 +1,21 @@
+package at.ac.fhcampuswien.fhmdb.models;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+//Recep To Do : Connect it with the Button
+public class MovieSorter {
+    public List<Movie> sortMovies(List<Movie> movies, SortState sortOrder) {
+        List<Movie> sortedMovies = new ArrayList<>(movies);
+
+        // Sort the movies based on the title in ascending or descending order
+        if (sortOrder == SortState.ASCENDING) {
+            Collections.sort(sortedMovies, Comparator.comparing(Movie::getTitle, String.CASE_INSENSITIVE_ORDER));
+        } else {
+            Collections.sort(sortedMovies, Comparator.comparing(Movie::getTitle, String.CASE_INSENSITIVE_ORDER).reversed());
+        }
+
+        return sortedMovies;
+    }
+
+}
